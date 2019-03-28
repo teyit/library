3e0dc63a4f987c4359d10d4bdb52ddd6b8bf7257
Can Çitoğlu <cancitoglu@gmail.com>
2019-03-28 09:48:16 +0300

    add dotenv

--- server/index.js
+++ server/index.js
@@ -1,77 +1,78 @@
-'use strict'
-const path = require('path')
+"use strict";
+require("dotenv").config();
+const path = require("path");
 
-const express = require('express')
-const csp = require('helmet-csp')
+const express = require("express");
+const csp = require("helmet-csp");
 
-const {middleware: cache} = require('./cache')
-const {getMeta} = require('./list')
-const {allMiddleware, requireWithFallback} = require('./utils')
-const userInfo = require('./routes/userInfo')
-const pages = require('./routes/pages')
-const categories = require('./routes/categories')
-const playlists = require('./routes/playlists')
-const readingHistory = require('./routes/readingHistory')
-const errorPages = require('./routes/errors')
+const { middleware: cache } = require("./cache");
+const { getMeta } = require("./list");
+const { allMiddleware, requireWithFallback } = require("./utils");
+const userInfo = require("./routes/userInfo");
+const pages = require("./routes/pages");
+const categories = require("./routes/categories");
+const playlists = require("./routes/playlists");
+const readingHistory = require("./routes/readingHistory");
+const errorPages = require("./routes/errors");
 
-const userAuth = requireWithFallback('userAuth')
-const customCsp = requireWithFallback('csp')
+const userAuth = requireWithFallback("userAuth");
+const customCsp = requireWithFallback("csp");
 
-const app = express()
+const app = express();
 
-const {preload, postload} = allMiddleware
+const { preload, postload } = allMiddleware;
 
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, '../layouts'))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../layouts"));
 
-app.get('/healthcheck', (req, res) => {
-  res.send('OK')
-})
+app.get("/healthcheck", (req, res) => {
+  res.send("OK");
+});
 
-app.use(csp({directives: customCsp}))
-app.use(userAuth)
+app.use(csp({ directives: customCsp }));
+app.use(userAuth);
 
-preload.forEach((middleware) => app.use(middleware))
+preload.forEach(middleware => app.use(middleware));
 
-app.use(userInfo)
+app.use(userInfo);
 
 // serve all files in the public folder
-app.use('/assets', express.static(path.join(__dirname, '../public')))
+app.use("/assets", express.static(path.join(__dirname, "../public")));
 
 // strip trailing slashes from URLs
 app.get(/(.+)\/$/, (req, res, next) => {
-  res.redirect(req.params[0])
-})
+  res.redirect(req.params[0]);
+});
 
-app.get('/view-on-site/:docId', (req, res, next) => {
-  const {docId} = req.params
-  const doc = getMeta(docId)
+app.get("/view-on-site/:docId", (req, res, next) => {
+  const { docId } = req.params;
+  const doc = getMeta(docId);
 
-  if (!doc) return next(Error('Not found'))
+  if (!doc) return next(Error("Not found"));
 
-  res.redirect(doc.path)
-})
+  res.redirect(doc.path);
+});
 
 // main pages
-app.use(readingHistory.middleware)
+app.use(readingHistory.middleware);
 
 // don't cache pages client-side to ensure browser always gets latest revision
 app.use((req, res, next) => {
-  res.set('Cache-Control', 'no-cache')
-  next()
-})
+  res.set("Cache-Control", "no-cache");
+  next();
+});
 
-app.use(pages)
-app.use(cache)
+app.use(pages);
+app.use(cache);
 
 // category pages will be cache busted when their last updated timestamp changes
-app.use(categories)
-app.use(playlists)
+app.use(categories);
+app.use(playlists);
 
-postload.forEach((middleware) => app.use(middleware))
+postload.forEach(middleware => app.use(middleware));
 
 // error handler for rendering the 404 and 500 pages, must go last
-app.use(errorPages)
-app.listen(parseInt(process.env.PORT || '3000', 10))
+app.use(errorPages);
+app.listen(parseInt(process.env.PORT || "3000", 10));
 
-module.exports = app
+module.exports = app;
